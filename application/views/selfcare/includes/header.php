9c07347f6296169c119d26b7eb175bee8afbde29
Mudassar <mudassar.khani@attech-ltd.com>
2019-10-16 12:55:54 +0500

    header link removal

--- application/views/selfcare/includes/header.php
+++ application/views/selfcare/includes/header.php
@@ -19,14 +19,6 @@
                 <button class="db-dropdown dropdown-toggle" id="menu1" type="button" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-angle-down"></i></button>
                     <ul class="dropdown-menu dropdown-list" role="menu" aria-labelledby="menu1" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 70px, 0px); top: 0px; left: 0px; will-change: transform;">
 
-
-
-                        <li role="presentation">
-                            <a role="menuitem" tabindex="-1" href="https://app.dr-iq.com/secure/" class="dropedown-dashboard-icon"><span class="dropedown-list">My Dashboard</span></a>
-                        </li>
-
-                        
-
                         <li role="presentation" style="border-bottom:0px;">
                             <a role="menuitem" tabindex="-1" href="<?php echo base_url().'admin/logout'?>" class="dropedown-logout-icon">
                                 <span class="dropedown-list">Logout</span>
