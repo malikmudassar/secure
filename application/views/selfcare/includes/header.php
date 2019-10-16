b53b9a935e00d7389f6ef586a4867be6a9517567
Mudassar <mudassar.khani@attech-ltd.com>
2019-10-16 13:26:40 +0500

    header image

--- application/views/selfcare/includes/header.php
+++ application/views/selfcare/includes/header.php
@@ -3,10 +3,10 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top border-seprater">
 
         <div class="container">
-
-            <a class="navbar-brand" href="">
-                <span style="color:black">Triage Builder</span>
-            </a>
+            <img src="<?php echo ASSET_URL.'assets/img/at-logo.png'?>" style="heigh:133px" />
+            <div >
+                <h2 style="font-family:Arial">Triage Builder</h2>
+            </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -16,7 +16,7 @@
             </div>
 
             <div class="dropdown profile-right">
-                <button class="db-dropdown dropdown-toggle" id="menu1" type="button" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-angle-down"></i></button>
+                <button class="db-dropdown dropdown-toggle" id="menu1" type="button" data-toggle="dropdown" aria-expanded="false">Account <i class="fa fa-angle-down"></i></button>
                     <ul class="dropdown-menu dropdown-list" role="menu" aria-labelledby="menu1" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 70px, 0px); top: 0px; left: 0px; will-change: transform;">
 
                         <li role="presentation" style="border-bottom:0px;">
