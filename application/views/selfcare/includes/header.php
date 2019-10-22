c038844d6315fceef3e11a29f55d651fcba4bbeb
Mudassar <mudassar.khani@attech-ltd.com>
2019-10-22 18:01:06 +0500

    changes

--- application/views/selfcare/includes/header.php
+++ application/views/selfcare/includes/header.php
@@ -8,32 +8,7 @@
                 <h1 style="font-family:lato; color:#1556e8"><b>EZ</b> Triage</h1>
             </div>
             <img src="<?php echo ASSET_URL.'assets/img/at-logo.png'?>" style="heigh:133px; float:right" />
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <?php if($this->session->userdata['name']){?>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                
-            </div>
-
-            <div class="dropdown profile-right">
-                <button class="db-dropdown dropdown-toggle" id="menu1" type="button" data-toggle="dropdown" aria-expanded="false">Account <i class="fa fa-angle-down"></i></button>
-                    <ul class="dropdown-menu dropdown-list" role="menu" aria-labelledby="menu1" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 70px, 0px); top: 0px; left: 0px; will-change: transform;">
-
-                        <li role="presentation" style="border-bottom:0px;">
-                            <a role="menuitem" tabindex="-1" href="<?php echo base_url().'admin/logout'?>" class="dropedown-logout-icon">
-                                <span class="dropedown-list">Logout</span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-
-
-                <!--notifications--->
-
-            </div>
-<?php }?>
+            
 
 
 </nav>
