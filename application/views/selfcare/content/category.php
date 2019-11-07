5e4cbd9122b568cb8a7a1f4390890fda8091b065
Mudassar <mudassar.khani@attech-ltd.com>
2019-11-07 12:57:07 +0500

    landing

--- application/views/selfcare/content/category.php
+++ application/views/selfcare/content/category.php
@@ -15,7 +15,7 @@
     			<div class="icon-path">
     				<img src="<?php echo ASSET_URL?>assets/img/pathways/Lifestyle.svg" alt="icon" class="img-fluid">
     			</div>
-    			<div style="display: inline-block;">
+    			<div style="display: inline-block; padding-top:7px">
     				<a href="<?php echo base_url().'selfcare/online/'?>"><h3>Life Style</h3></a>
     			</div>
     	       <img src="<?php echo ASSET_URL?>assets/img/pathways/arrowRight.svg" alt="" class="img-fluid arrowPath" style="float: right;">
@@ -27,7 +27,7 @@
     			<div class="icon-path">
     				<img src="<?php echo ASSET_URL?>assets/img/pathways/Online-Consultation.svg" alt="icon" class="img-fluid">
     			</div>
-    			<div style="display: inline-block;">
+    			<div style="display: inline-block;padding-top:7px">
     				<a href="<?php echo base_url().'selfcare/consultations/'?>"><h3>Online Consultations</h3></a>
     			</div>
     	       <img src="<?php echo ASSET_URL?>assets/img/pathways/arrowRight.svg" alt="" class="img-fluid arrowPath" style="float: right;">
