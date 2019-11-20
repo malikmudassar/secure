bc6297a24d082d50776eeddf186588d1da857e98
Mudassar <mudassar.khani@attech-ltd.com>
2019-11-20 13:14:46 +0500

    tinymCE

--- application/views/selfcare/content/edit_question.php
+++ application/views/selfcare/content/edit_question.php
@@ -4,19 +4,17 @@
 <!--end navigation bar -->
 </div>
 <div class="container">
-    <div class="pathwaysContent maincontent-space" style="border:1px solid #ccc; background-color:#f0eded; margin-bottom:0px; padding-left:40px; padding-right:20px; margin-top:50px;">
+    <div class="pathwaysContent maincontent-space" style="margin-bottom:0px; padding-left:40px; padding-right:20px; margin-top:50px;">
         <div class="pathways" >
             
             <div class="login-form" >
                     <form action="" method="post">
                         <div class="form-group">
                             <label><b>Statement</b></label>
-                            <textarea class="form-control" name="statement"><?php echo $question['statement']?></textarea>
+                            <textarea id='myTextarea' class="form-control" name="statement"><?php echo $question['statement']?></textarea>
                         </div>  
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary"
-                            style=""
-                            >
+                            <button type="submit" class="btn btn-primary">
                                 Update
                             </button>
                         </div>                   
