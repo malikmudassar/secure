79ecc75757fcc51ca8a1fda3b6ba852fb3f16a5f
Mudassar <mudassar.khani@attech-ltd.com>
2019-12-03 18:48:42 +0500

    delete query fix

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -8099,9 +8099,7 @@ class Admin_model extends CI_Model {
 
     public function finish_pw($pw, $user_id)
     {
-        $this->db->delete()->from('pathway_steps')
-        ->where('pathway', $pw)
-        ->where('user_id', $user_id);
+        $this->db->query('delete from pathway_steps where pathway='.$pathway.' and user_id='.$user_id);
     }
 
 }
